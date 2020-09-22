cbf7d494f72bdce214a0e0f2a6acebaa91ab7695
Bruno Martins <bruno.sousa.martins@gmail.com>
2020-09-22 17:09:32 +0100

    SellersController

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -32,8 +32,8 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            var department = await _context.Department
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var department = await _context.Department.FirstOrDefaultAsync(m => m.Id == id);
+
             if (department == null)
             {
                 return NotFound();
